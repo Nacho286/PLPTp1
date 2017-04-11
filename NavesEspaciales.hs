78e4e5bc2f529bd56b7e77323bd8418156bf1a3f
Nacho286 <igna_nacho286@hotmail.com>
2017-04-10 23:58:49 -0300

    cambios

--- NavesEspaciales.hs
+++ NavesEspaciales.hs
@@ -1,5 +1,5 @@
-module NavesEspaciales (Componente(Contenedor, Motor, Escudo, Cañón), NaveEspacial(Módulo, Base), Dirección(Babor, Estribor), TipoPeligro(Pequeño, Grande, Torpedo), Peligro, foldNave, capacidad, poderDeAtaque, puedeVolar, mismoPotencial, mayorCapacidad, transformar, impactar, maniobrar, pruebaDeFuego, componentesPorNivel, dimensiones) where
-
+module NavesEspaciales (Componente(Contenedor, Motor, Escudo, Cañón), NaveEspacial(Módulo, Base), Dirección(Babor, Estribor), TipoPeligro(Pequeño, Grande, Torpedo), Peligro,foldNave, capacidad) where
+-- , poderDeAtaque, puedeVolar, mismoPotencial, mayorCapacidad, transformar, impactar, maniobrar, pruebaDeFuego, componentesPorNivel, dimensiones
 data Componente = Contenedor | Motor | Escudo | Cañón deriving (Eq, Show)
 
 data NaveEspacial = Módulo Componente NaveEspacial NaveEspacial | Base Componente deriving Eq
@@ -12,36 +12,35 @@ type Peligro = (Dirección, Int, TipoPeligro)
 
 instance Show NaveEspacial where
   show = ("\n" ++) . (padNave 0 0 False)
-  
+
 padNave nivel acum doPad (Base c) = (if doPad then pad (4*nivel + acum) else "") ++ show c
-padNave nivel acum doPad (Módulo x i d) = (if doPad then pad (4*nivel + acum) else "") ++ show x ++ 
-					  pad 4 ++ padNave (nivel+1) (acum+l) False i ++ "\n" ++
-					  padNave (nivel+1) (acum+l) True d where l = length $ show x
+padNave nivel acum doPad (Módulo x i d) = (if doPad then pad (4*nivel + acum) else "") ++ show x ++
+            pad 4 ++ padNave (nivel+1) (acum+l) False i ++ "\n" ++
+            padNave (nivel+1) (acum+l) True d where l = length $ show x
 
 pad :: Int -> String
 pad i = replicate i ' '
 
 --Ejercicio 1
 foldNave :: (Componente->a)->(a->a->a->a)->NaveEspacial->a
-foldNave f g (Base c) = f c 
-foldNave f g (Módulo c n1 n2) = g f c (foldNave f g n1) (foldNave f g n2)
+foldNave f g (Base c) = f c
+foldNave f g (Módulo c n1 n2) = g (f c) (foldNave f g n1) (foldNave f g n2)
 
 --Ejercicio 2
 capacidad :: NaveEspacial -> Int
-capacidad = foldNave (+1) sumarTres
-				where
-					sumarTres x y z = x+y+z
-
-poderDeAtaque :: NaveEspacial -> Int
-poderDeAtaque = foldNave esCañon sumarTres
-				where
-					esCañon x = if x==Cañón then 1 else 0
-					sumarTres x y z = x+y+z
-
-puedeVolar :: NaveEspacial -> Bool
-puedeVolar = foldNave (==Motor) O
-				where
-					O x y z= x || y || z
+capacidad = foldNave const(1) sumarTres
+ 				where sumarTres x y z = x+y+z
+--
+-- poderDeAtaque :: NaveEspacial -> Int
+-- poderDeAtaque = foldNave esCañon sumarTres
+-- 				where
+-- 					esCañon x = if x==Cañón then 1 else 0
+-- 					sumarTres x y z = x+y+z
+--
+-- puedeVolar :: NaveEspacial -> Bool
+-- puedeVolar = foldNave (==Motor) disyuncion
+-- 				where
+-- 					disyuncion x y z= x || y || z
 
 --mismoPotencial :: NaveEspacial -> NaveEspacial -> Bool
 --mismoPotencial = undefined
